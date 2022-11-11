0c0325a1ec571cd6adde0808d924c15cf7fbfc7b
Reza Yogaswara <reza.yoga@gmail.com>
2022-11-11 09:56:32 +0700

    Updating

--- project/config.py
+++ project/config.py
@@ -10,7 +10,7 @@ class BaseConfig:
 	# "redis://reza:reza1985@rezayogaswara.com:6379/15")
 	result_backend: str = os.environ.get("CELERY_RESULT_BACKEND", "redis://reza:reza1985@rezayogaswara.com:6379/15")
 
-	RABBITMQ_URL: str = os.environ.get("RABBITMQ_URL", "amqp://reza:reza@rezayogaswara.com:5672")
+	RABBITMQ_URL: str = os.environ.get("RABBITMQ_URL", "amqp://rnd:password@rnd.coster.id:5672")
 	RABBITMQ_SERVICE_PUBLISH_QUEUE_NAME = os.environ.get("RABBITMQ_SERVICE_PUBLISH_QUEUE_NAME",
 	                                                     "service.queue.payload.publish")
 	RABBITMQ_SERVICE_CONSUME_QUEUE_NAME = os.environ.get("RABBITMQ_SERVICE_CONSUME_QUEUE_NAME",
@@ -28,7 +28,7 @@ class ProductionConfig(BaseConfig):
 
 
 class TestingConfig(BaseConfig):
-	CELERY_BROKER_URL: str = os.environ.get("CELERY_BROKER_URL", "amqp://reza:reza@rezayogaswara.com:5672")
+	CELERY_BROKER_URL: str = os.environ.get("CELERY_BROKER_URL", "amqp://rnd:password@rnd.coster.id:5672")
 	CELERY_RESULT_BACKEND: str = os.environ.get("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/15")
 
 
