9f3325808e0851c18b380cd5494f5f28f45df999
Reza Yogaswara <reza.yoga@gmail.com>
2022-11-11 10:57:30 +0700

    Updating

--- project/config.py
+++ project/config.py
@@ -11,10 +11,6 @@ class BaseConfig:
 	result_backend: str = os.environ.get("CELERY_RESULT_BACKEND", "redis://reza:reza1985@rezayogaswara.com:6379/15")
 
 	RABBITMQ_URL: str = os.environ.get("RABBITMQ_URL", "amqp://rnd:password@rnd.coster.id:5672")
-	RABBITMQ_SERVICE_PUBLISH_QUEUE_NAME = os.environ.get("RABBITMQ_SERVICE_PUBLISH_QUEUE_NAME",
-	                                                     "service.queue.payload.publish")
-	RABBITMQ_SERVICE_CONSUME_QUEUE_NAME = os.environ.get("RABBITMQ_SERVICE_CONSUME_QUEUE_NAME",
-	                                                     "service.queue.payload.consume")
 
 	RABBITMQ_SERVICE_QUEUE_NAME = os.environ.get("RABBITMQ_SERVICE_QUEUE_NAME", "service.queue.payload")
 
