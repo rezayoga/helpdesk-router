162f0e9bb4c81a19d809f4eedb6a2e9545f22964
Reza Yogaswara <reza.yoga@gmail.com>
2022-11-14 13:08:50 +0700

    Updating

--- project/__init__.py
+++ project/__init__.py
@@ -4,6 +4,7 @@ import logging.config
 import os
 from typing import Optional
 
+from urllib.parse import quote_plus
 import aioredis
 from aio_pika.abc import AbstractIncomingMessage
 from fastapi import FastAPI
@@ -20,7 +21,8 @@ from project.config import settings
 from project.core import WebSocketManager, PikaClient
 from project.schemas import User as UserSchema, Payload as PayloadSchema, UserValidation
 
-redis = aioredis.from_url(os.environ.get('result_backend', "redis://reza:reza1985@103.41.204.222:6379/15"))
+# redis = aioredis.from_url(os.environ.get('result_backend', "redis://reza:reza1985@103.41.204.222:6379/15"))
+redis = aioredis.from_url(f"redis://:{quote_plus('Cost3rv3Redi5P@ssw0rd').replace('%', '%%')}@192.168.217.2:6379/0")
 # setup loggers
 logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
 
