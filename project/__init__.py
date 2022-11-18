b01e1a7c54f2d01a95c1faf2cb1e13b356b06411
Reza Yogaswara <reza.yoga@gmail.com>
2022-11-18 17:55:46 +0700

    Updating

--- project/__init__.py
+++ project/__init__.py
@@ -130,10 +130,10 @@ def create_app() -> FastAPI:
 			if validated_user.is_validated:
 				self.user_id = validated_user.user.user.id
 				logger.info(f"User {self.user_id} connected")
-				await websocket.send_json(
-					{"type": "WEBSOCKET_JOIN", "data": {"id": self.user_id,
-					                                    "client": jsonable_encoder(validated_user.user)}}
-				)
+				# await websocket.send_json(
+				# 	{"type": "WEBSOCKET_JOIN", "data": {"id": self.user_id,
+				# 	                                    "client": jsonable_encoder(validated_user.user)}}
+				# )
 				# await self.websocket_manager.broadcast_user_joined(self.user_id)
 				self.websocket_manager.add_user(self.user_id, validated_user.user.user.id, websocket)
 			else:
@@ -175,7 +175,7 @@ def create_app() -> FastAPI:
 				return UserValidation(is_validated=True, user=u)
 			return UserValidation(is_validated=False, user=None)
 
-	async def log_incoming_message(message: dict):
+	def log_incoming_message(message: dict):
 		# inspect(message, methods=False)
 		if wm is not None:
 			key_list = wm.users.keys()
@@ -188,14 +188,14 @@ def create_app() -> FastAPI:
 			if payload.broadcast:
 				inspect(key_list, methods=False)
 				inspect(payload.broadcast, methods=False)
-				await wm.broadcast_all_users(jsonable_encoder(payload))
+				wm.broadcast_all_users(jsonable_encoder(payload))
 			else:
 				r = sorted(payload.recipients)
 				active_user_in_websocket = sorted(key_list)
 				intersection = set(r).intersection(set(active_user_in_websocket))
 				if len(intersection) > 0:
 					for user_id in intersection:
-						await wm.broadcast_by_user_id(user_id, jsonable_encoder(payload))
+						wm.broadcast_by_user_id(user_id, jsonable_encoder(payload))
 
 	pika_client = PikaClient(log_incoming_message)
 
