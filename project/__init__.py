e01141055f6270faff293521dcc8a57875ea8b64
Reza Yogaswara <reza.yoga@gmail.com>
2022-11-22 09:17:38 +0700

    Updating

--- project/__init__.py
+++ project/__init__.py
@@ -143,10 +143,9 @@ def create_app() -> FastAPI:
 				await websocket.close()
 
 		async def on_disconnect(self, _websocket: WebSocket, _close_code: int):
-			# if self.user_id:
-			# 	self.websocket_manager.remove_user(self.user_id)
-			# 	await self.websocket_manager.broadcast_user_left(self.user_id)
-			pass
+			if self.user_id:
+				self.websocket_manager.remove_user(self.user_id)
+				await self.websocket_manager.broadcast_user_left(self.user_id)
 
 		async def on_receive(self, _websocket: WebSocket, payload: PayloadSchema):
 			if self.user_id is None:
